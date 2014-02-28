6a5ee7d7ba3bc5b7e81ef11ed7aba0c5cfc8934b
Tobias Olausson <tobsan@gmail.com>
2014-02-28 14:18:00 +0100

    updated log and minor thoughts entered into measuring module

--- src/Measuring.hs
+++ src/Measuring.hs
@@ -11,14 +11,26 @@ import CnfTablesJava
 import Data.Matrix.Quad
 import Algebra.RingUtils
 
-instance Monoid (Mat x y a) where
-    mempty = Zero
-    t0 `mappend` t1 = undefined -- chop!
+-- 
+-- Should it be Mat x y or SomeTri? SomeTri seems more reasonable in a way. 
+-- 
 
-instance Measured (Mat x x IntToken) IntToken where
+instance Monoid (SomeTri a) where
+    mempty = Zero
+    t0 `mappend` t1 = \b -> merge b t0 t1
+
+instance Measured (SomeTri IntToken) IntToken where
     measure tok = One tok
 
+-- This is the progress:
+-- Lexer input is Char, measured to IntToken
+-- IntToken has to be converted into Tokens
+-- 
+-- Tokens is the input to CnfTables, which can convert them to a Pair of
+-- category and AST-ADT.
+--
+-- The category/AST-ADT-pairs can then be input to be converted to matricies in
+-- the Quad module of BNFC.
+--
+-- Char -> IntToken -> Token -> Pair [(Category, Any *)]
 
---
--- lexer input >>= \tree -> parser tree >>= \abssyn
---
