ddf8eea65bdcb803c2a2fce1b7fc13f0c9d305a8
Tobias Olausson <tobsan@gmail.com>
2014-03-08 17:39:44 +0100

    probably working version of measure from IntToken -> Token -> CATEGORY now. needs testing.

--- src/Measuring.hs
+++ src/Measuring.hs
@@ -1,9 +1,11 @@
-{-# LANGUAGE MultiParamTypeClasses, TypeSynonymInstances, FlexibleInstances, UndecidableInstances, DataKinds #-}
+{-# LANGUAGE MultiParamTypeClasses, TypeSynonymInstances, FlexibleContexts,
+FlexibleInstances, GADTs, UndecidableInstances, DataKinds #-}
 
 module Measuring where
 
+import GHC.Prim
 import Data.FingerTree (Measured, measure)
-import Data.Monoid
+import Data.Monoid hiding (Any)
 import System.IO.Unsafe
 import System.Random
 
@@ -12,41 +14,25 @@ import CnfTablesJavaletteLight
 import Data.Matrix.Quad
 import Algebra.RingUtils
 
--- 
--- TODO: Change Token to [(Category,Any)] or something along those lines. 
---
-
-instance Monoid (SomeTri Token) where
+instance Monoid (SomeTri [(CATEGORY,Any)]) where
     mempty = T Leaf' (Zero :/: Zero)
     t0 `mappend` t1 = merge True t0 t1
     -- FIXME: Change to (unsafePerformIO randomIO)
 
-instance Measured (SomeTri Token) IntToken where
+instance Measured (SomeTri [(CATEGORY,Any)]) IntToken where
     -- Note: place the token just above the diagonal
-    measure tok = T s (q :/: q)
-      where s = Bin' 0 Leaf' Leaf'
-            q = quad Zero t Zero Zero
-            t = case intToToken tok of
-                Nothing  -> Zero
-                Just tok -> One tok
+    measure tok = T (Bin' 0 Leaf' Leaf') (q True :/: q False)
+      where 
+        q b = quad Zero (t b) Zero Zero
+        t b = case intToToken tok of
+            Nothing  -> Zero
+            Just tok -> One $ (if b then leftOf else rightOf) $ tokenToCats b tok
 
-
+-- None and Skip are just discarded, as seen in measureToTokens in LexGen
 intToToken :: IntToken -> Maybe Token
 intToToken (Token lex acc) = case acc of
     AlexAccNone -> Nothing
     AlexAccSkip -> Nothing
     AlexAcc f   -> Just $ f (Pn 0 1 1) lex -- dummy position for now
 
-
--- This is the progress:
--- Lexer input is Char, measured to IntToken
--- IntToken has to be converted into Tokens - HOW?
--- 
--- Tokens is the input to CnfTables, which can convert them to a Pair of
--- category and AST-ADT.
---
--- The category/AST-ADT-pairs can then be input to be converted to matricies in
--- the Quad module of BNFC.
---
--- Char -> IntToken -> Token -> Pair [(Category, Any *)]
 
