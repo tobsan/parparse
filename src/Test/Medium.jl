23a8d90dc011b9ad30667ad127a466b34ab0a2e0
Tobias Olausson <tobsan@gmail.com>
2014-03-17 17:13:06 +0100

    added javalette test programs

---
+++ src/Test/Medium.jl
@@ -0,0 +1,23 @@
+int main() {
+    int p;
+    int x;
+    x = 2;
+    p = 2;
+    while(p < 5) {
+        x = x * x;
+        p = p + 1;
+    }
+    int t;
+    int s;
+    s = 1;
+    t = 10 * 2;
+    while(t < 3) {
+        while(s < 5) {
+            x = 3;
+            s = 5;
+        }
+    }
+    t = 0;
+    p = 1;
+}
+
