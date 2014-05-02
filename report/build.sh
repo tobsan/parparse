0f2a875b16c2afae7aade287db5bff8094fe7c83
Tobias Olausson <tobsan@gmail.com>
2014-05-02 15:09:22 +0200

    updated report

--- report/build.sh
+++ report/build.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
+#
+# Small script to rebuild a pdf from LaTeX and BibTeX source automatically on
+# file change. Requires the inotify-tools package.
+#
 
 REPORT="report.tex"
 BIBL="report.aux"
 
-while inotifywait -q $REPORT
+while inotifywait -q "$REPORT"
 do
-    pdflatex $REPORT
-    bibtex $BIBL
-    pdflatex $REPORT
-    pdflatex $REPORT
+    sleep 1s
+    pdflatex "$REPORT"
+    bibtex "$BIBL"
+    pdflatex "$REPORT"
+    pdflatex "$REPORT"
     echo "*********************************"
     echo "Regenerated pdf output from LaTeX"
     echo "*********************************"
