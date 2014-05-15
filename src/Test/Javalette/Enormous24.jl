33cc35132dcebf3ed47e247ca880b891d16dd52e
Tobias Olausson <tobsan@gmail.com>
2014-05-15 10:56:06 +0200

    another testfile, reasonably small

---
+++ src/Test/Javalette/Enormous24.jl
@@ -0,0 +1,759 @@
+__BEGIN_PROGRAM
+
+int main() {
+    int p;
+    int x;
+    x = 2;
+    p = 2;
+    while(p < 5) {
+        x = x * x;
+        p = p + 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(s < t) {
+            s = s+2;
+            t = t+1;
+        }
+    }
+}
+
+int main() {
+    int p;
+    int x;
+    x = 2;
+    p = 2;
+    while(p < 5) {
+        x = x * x;
+        p = p + 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        x = 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(s < t) {
+            s = s+2;
+            t = t+1;
+        }
+    }
+}
+
+int main() {
+    int p;
+    int x;
+    x = 2;
+    p = 2;
+    while(p < 5) {
+        x = x * x;
+        p = p + 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(s < t) {
+            s = s+2;
+            t = t+1;
+        }
+    }
+}
+
+int main() {
+    int p;
+    int x;
+    x = 2;
+    p = 2;
+    while(p < 5) {
+        x = x * x;
+        p = p + 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        x = 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(s < t) {
+            s = s+2;
+            t = t+1;
+        }
+    }
+}
+
+int main() {
+    int p;
+    int x;
+    x = 2;
+    p = 2;
+    while(p < 5) {
+        x = x * x;
+        p = p + 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(s < t) {
+            s = s+2;
+            t = t+1;
+        }
+    }
+}
+
+int main() {
+    int p;
+    int x;
+    x = 2;
+    p = 2;
+    while(p < 5) {
+        x = x * x;
+        p = p + 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        x = 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(s < t) {
+            s = s+2;
+            t = t+1;
+        }
+    }
+}
+
+int main() {
+    int p;
+    int x;
+    x = 2;
+    p = 2;
+    while(p < 5) {
+        x = x * x;
+        p = p + 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(s < t) {
+            s = s+2;
+            t = t+1;
+        }
+    }
+}
+
+int main() {
+    int p;
+    int x;
+    x = 2;
+    p = 2;
+    while(p < 5) {
+        x = x * x;
+        p = p + 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        x = 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(s < t) {
+            s = s+2;
+            t = t+1;
+        }
+    }
+}
+
+int main() {
+    int p;
+    int x;
+    x = 2;
+    p = 2;
+    while(p < 5) {
+        x = x * x;
+        p = p + 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(s < t) {
+            s = s+2;
+            t = t+1;
+        }
+    }
+}
+
+int main() {
+    int p;
+    int x;
+    x = 2;
+    p = 2;
+    while(p < 5) {
+        x = x * x;
+        p = p + 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        x = 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(s < t) {
+            s = s+2;
+            t = t+1;
+        }
+    }
+}
+
+int main() {
+    int p;
+    int x;
+    x = 2;
+    p = 2;
+    while(p < 5) {
+        x = x * x;
+        p = p + 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(s < t) {
+            s = s+2;
+            t = t+1;
+        }
+    }
+}
+
+int main() {
+    int p;
+    int x;
+    x = 2;
+    p = 2;
+    while(p < 5) {
+        x = x * x;
+        p = p + 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        x = 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(s < t) {
+            s = s+2;
+            t = t+1;
+        }
+    }
+}
+
+int main() {
+    int p;
+    int x;
+    x = 2;
+    p = 2;
+    while(p < 5) {
+        x = x * x;
+        p = p + 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(s < t) {
+            s = s+2;
+            t = t+1;
+        }
+    }
+}
+
+int main() {
+    int p;
+    int x;
+    x = 2;
+    p = 2;
+    while(p < 5) {
+        x = x * x;
+        p = p + 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        x = 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(s < t) {
+            s = s+2;
+            t = t+1;
+        }
+    }
+}
+
+int main() {
+    int p;
+    int x;
+    x = 2;
+    p = 2;
+    while(p < 5) {
+        x = x * x;
+        p = p + 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(s < t) {
+            s = s+2;
+            t = t+1;
+        }
+    }
+}
+
+int main() {
+    int p;
+    int x;
+    x = 2;
+    p = 2;
+    while(p < 5) {
+        x = x * x;
+        p = p + 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        x = 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(s < t) {
+            s = s+2;
+            t = t+1;
+        }
+    }
+}
+
+int main() {
+    int p;
+    int x;
+    x = 2;
+    p = 2;
+    while(p < 5) {
+        x = x * x;
+        p = p + 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(s < t) {
+            s = s+2;
+            t = t+1;
+        }
+    }
+}
+
+int main() {
+    int p;
+    int x;
+    x = 2;
+    p = 2;
+    while(p < 5) {
+        x = x * x;
+        p = p + 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        x = 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(s < t) {
+            s = s+2;
+            t = t+1;
+        }
+    }
+}
+
+int main() {
+    int p;
+    int x;
+    x = 2;
+    p = 2;
+    while(p < 5) {
+        x = x * x;
+        p = p + 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(s < t) {
+            s = s+2;
+            t = t+1;
+        }
+    }
+}
+
+int main() {
+    int p;
+    int x;
+    x = 2;
+    p = 2;
+    while(p < 5) {
+        x = x * x;
+        p = p + 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        x = 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(s < t) {
+            s = s+2;
+            t = t+1;
+        }
+    }
+}
+
+int main() {
+    int p;
+    int x;
+    x = 2;
+    p = 2;
+    while(p < 5) {
+        x = x * x;
+        p = p + 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(s < t) {
+            s = s+2;
+            t = t+1;
+        }
+    }
+}
+
+int main() {
+    int p;
+    int x;
+    x = 2;
+    p = 2;
+    while(p < 5) {
+        x = x * x;
+        p = p + 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        x = 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(s < t) {
+            s = s+2;
+            t = t+1;
+        }
+    }
+}
+
+int main() {
+    int p;
+    int x;
+    x = 2;
+    p = 2;
+    while(p < 5) {
+        x = x * x;
+        p = p + 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(s < t) {
+            s = s+2;
+            t = t+1;
+        }
+    }
+}
+
+int main() {
+    int p;
+    int x;
+    x = 2;
+    p = 2;
+    while(p < 5) {
+        x = x * x;
+        p = p + 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        x = 1;
+        while(p < 5) {
+            x = x * x;
+            p = p + 1;
+        }
+        int t;
+        int s;
+        s = 1;
+        t = 10 * 2;
+        while(s < t) {
+            s = s+2;
+            t = t+1;
+        }
+    }
+}
+
+__END_PROGRAM
