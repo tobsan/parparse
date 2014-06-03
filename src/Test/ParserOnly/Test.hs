ccdeb7d6380eafdeed0960cd50888f8e81881a11
Tobias Olausson <tobsan@gmail.com>
2014-06-03 14:33:52 +0200

    test program comparing incremental and regular lexer when combining with incremental parser. Result is......interesting.

--- src/Test/ParserOnly/Test.hs
+++ src/Test/ParserOnly/Test.hs
@@ -4,18 +4,43 @@ import System.Environment
 import Data.FingerTree (fromList)
 import Data.Maybe
 import Control.Monad (forM_)
-import PrintC
-import AbsC
-import LexC as L
-import CYKParser as P
+-- Non-incremental lexing
+import qualified PrintC as Print
+import qualified AbsC as Abc
+import qualified LexC as L
+import qualified CYKParser as P
+-- Incremental lexing
+import qualified PrintCInc as PrintInc
+import qualified AbsCInc as AbcInc
+import qualified LexCInc as LI
+import qualified CYKParserInc as PI
+
+testInc :: String -> IO ()
+testInc str = 
+    forM_ ast $ \(_,as,_) -> do
+        forM_ as $ \a ->
+            print $ PrintInc.printTree $ fromJust $ PI.getProgram $ a
+        putStrLn $ "Parse results: " ++ show (length as)
+  where 
+    ts = LI.tokens str
+    ast = PI.parse ts
+
+testReg :: String -> IO ()
+testReg str = 
+    forM_ ast $ \(_,as,_) -> do
+        forM_ as $ \a ->
+            print $ Print.printTree $ fromJust $ P.getProgram $ a
+        putStrLn $ "Parse results: " ++ show (length as)
+  where 
+    ts = L.tokens str
+    ast = P.parse $ fromList ts
 
 main :: IO ()
 main = do
     f:_ <- getArgs
     cs <- readFile f
-    let ts = L.tokens cs
-        ast = P.parse $ fromList ts
-    forM_ ast $ \(_,as,_) -> do
-        forM_ as $ \a ->
-            print $ printTree $ fromJust $ getProgram $ a
+    putStrLn "*** Testing with incremental lexer ***"
+    testInc cs
+    putStrLn "*** Testing with regular lexer ***"
+    testReg cs
 
